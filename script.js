110122be9da463460b1cb08b6ac502255394771e
itsjustinhurdawg <30875159+itsjustinhurdawg@users.noreply.github.com>
2017-08-14 09:23:14 -1000

    script.js
    created something a function called sayHello. After i created a variable to ask a user something.

---
+++ script.js
@@ -0,0 +1,10 @@
+function sayHello(){
+  //I created a function called sayHello//
+  var userName = prompt("What is your name");
+  var userNick = prompt("what is your nickname");
+  var userLast = prompt("what is your last name");
+  // I created a variable caleed userName and asked the user something//
+  window.alert("Hello" + " " + userName + " " + userNick + " " + userLast );
+  //I created a popup window telling the userName hello.//
+  
+}
