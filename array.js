61edd5bb44258a99cb2059e74ad53d1b4fe984e7
itsjustinhurdawg <30875159+itsjustinhurdawg@users.noreply.github.com>
2017-08-16 08:44:39 -1000

    array.gs
    i created an array in javascript

---
+++ array.js
@@ -0,0 +1,5 @@
+var favFoods = ["bacon", "Lettuce", "Tomato"]; // lists of foods in "favFoods"//
+
+var students = ["Michael", "Raene", "Chuck", "Ryan"]; // list of names in our class//
+
+window.alert(students[2] + " " +  favFoods[0]);
