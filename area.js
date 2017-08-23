e8524622c315be23da65c5b417285b3f3faa8ce0
itsjustinhurdawg <30875159+itsjustinhurdawg@users.noreply.github.com>
2017-08-23 08:44:18 -1000

    Create area.js
    using width and height to find the area.

---
+++ area.js
@@ -0,0 +1,6 @@
+function calculateArea(width,height){ //these match with the code on the bottom//
+var area = width * height; //because code, line 1 and line 5 matches, you can use this code to multiply 3 and 5//
+return area;
+}
+var wallOne = calculateArea(3,5); //these matches with the code, line 1//
+window.alert(wallOne);
