2888b4fbab026b693eaa61ea9e988e6f44c6614e
itsjustinhurdawg <30875159+itsjustinhurdawg@users.noreply.github.com>
2017-08-23 08:14:33 -1000

    random.js
    randomNumber.length

---
+++ random.js
@@ -0,0 +1,3 @@
+var randomNumber = ["10","20", "30", "40",];
+var number = Math.floor(Math.random() * randomNumber.length); // you use randomNumber.length because you might not know the length of the arrar
+window.alert(randomNumber[number]);
