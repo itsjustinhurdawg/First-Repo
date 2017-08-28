3f9fa20f2551be35d10ba423f0146df2adb5b503
itsjustinhurdawg <30875159+itsjustinhurdawg@users.noreply.github.com>
2017-08-28 08:43:26 -1000

    Create newName.js
    this code makes us change our name to whatever name we want.

---
+++ newName.js
@@ -0,0 +1,11 @@
+var newName = prompt("WHat is his first new name?");
+
+var students = {
+firstName: "Justin",
+lastName: "Hur",
+age: 17,
+grade:12
+}
+
+window.alert(students.firstName= newName);
+window.alert(students.firstName);// you use this code to check if the name actually changed.
