e788da46de6febfaffd8c12714e18e427f0cff24
itsjustinhurdawg <30875159+itsjustinhurdawg@users.noreply.github.com>
2017-09-06 09:20:11 -1000

    Update neverendingobject.js

--- neverendingobject.js
+++ neverendingobject.js
@@ -27,3 +27,24 @@ else{
 askAgain();
 }
 }
+
+// Updated to only have 3 tries.
+var stem = {
+  roomNumber: "F103",
+  students: 18
+};
+
+var userInput = prompt("roomNumber or students");
+var lowerCase = userInput.toLowerCase();
+if ((lowerCase === "roomNumber") || (lowerCase === "students")) {
+  window.alert(stem[lowerCase]);
+} 
+else {
+  for (i = 1; i < 3; i++) {
+  var userInput = prompt("roomNumber or students. You have tried" +i+"times");
+var lowerCase = userInput.toLowerCase();
+if ((lowerCase === "roomNumber") || (lowerCase === "students")) {
+  window.alert(stem[lowercase]);
+		} 
+  }
+}
