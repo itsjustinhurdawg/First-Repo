0bce71d6bc2590e8c9eabd6a6545cd150043cbb8
itsjustinhurdawg <30875159+itsjustinhurdawg@users.noreply.github.com>
2017-09-21 09:06:43 -1000

    Create mouseOverPositionChange.js
    in this code, when you hover your mouse over the picture of a corgi, the picture turns at a 45 degree angle and when you move your mouse out of the picture, it will go back -45 degree

---
+++ mouseOverPositionChange.js
@@ -0,0 +1,12 @@
+--HTML--
+
+<img src ="http://www.barnwoodaussies.net/litters/chaseXsky11/images/Copy-sit.jpg" id ="corgi" onmouseover="rotatePos()" onmouseout="rotateNeg()" width="50%">
+
+--JAVASCRIPT-- 
+
+function goAway(){
+document.getElementById('corgi').style.visibility ="hidden";
+ }
+function comeBack(){
+document.getElementById('corgi').style.visibility="visible"
+}
